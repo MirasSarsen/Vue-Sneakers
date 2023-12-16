832f67c9a71ad83af9adfcb71941e79b54059109
MirasSarsen <framekz001@gmail.com>
2023-12-16 23:23:15 +0600

    Initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  name: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
+})
+</script>
+
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img
+      :src="!isFavorite ? '../../public/like-1.svg' : '../../public/like-2.svg'"
+      alt="like"
+      class="absolute"
+      @click="onClickFavorite"
+    />
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ name }}</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }} руб.</b>
+      </div>
+
+      <img
+        @click="onClickAdd"
+        :src="!isAdded ? '../../public/plus.svg' : '../../public/checked.svg'"
+        alt="plus"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Card from '../components/Card.vue'
+
+defineProps({
+  items: Array
+})
+
+const onClickAdd = () => {
+  alert('Добавить')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :name="item.name"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
